58e8a98c26ffb9ff68868e5cb0f7cc9e5d4d4abd
AgusLez <agtlez@gmail.com>
2021-01-11 15:45:56 -0300

    minimal style changes

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,8 +1,8 @@
 function toggleMenu () {
   var item = document.getElementById('links');
   item.classList.toggle('open');
+
   var nav = document.getElementById('nav');
-  console.log(nav);
   nav.classList.toggle('open');
 }
 
