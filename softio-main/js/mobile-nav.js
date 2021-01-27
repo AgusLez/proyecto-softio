483a75f48eea339f8ba33210bae1c83b353bcb35
AgusLez <agtlez@gmail.com>
2021-01-27 12:34:41 -0300

    adjust the views

---
+++ softio-main/js/mobile-nav.js
@@ -0,0 +1,8 @@
+function toggleMenu () {
+  var item = document.getElementById('links');
+  item.classList.toggle('open');
+
+  var nav = document.getElementById('nav');
+  nav.classList.toggle('open');
+}
+
